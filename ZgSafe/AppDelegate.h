94aa66549dcf45444818b7ef60524335d85e552b
whoslie <chwpp223@163.com>
2017-05-18 20:16:00 +0800

    Initial Commit

---
+++ ZgSafe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZgSafe
+//
+//  Created by whoslie on 2017/5/18.
+//  Copyright © 2017年 whoslie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZgSafe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZgSafe
+//
+//  Created by whoslie on 2017/5/18.
+//  Copyright © 2017年 whoslie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
